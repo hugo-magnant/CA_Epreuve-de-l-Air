ba2dc4e1f6595c4e28736e2bc87bdb6718b095d6
Hugo Magnant <115119407+hugo-magnant@users.noreply.github.com>
2023-10-04 10:47:18 +0200

    Create air13.rb

---
+++ air13.rb
@@ -0,0 +1,8 @@
+# exo.rb
+
+def celebrate_victory(adjective)
+  puts "J'ai terminé l'Épreuve de l'Air et c'était #{adjective}."
+end
+
+# Vous pouvez remplacer 'facile' par n'importe quel adjectif de votre choix
+celebrate_victory('facile')
